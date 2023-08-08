60a215ba62e2b5ec7a8e52b9e84b36def33afba3
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-08 10:37:19 -0300

    VCTACION

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Layout from "./components/Layout";
+import Contacto from "./pages/Contacto.js";
 import Navigator from "./components/Navigator";
 import Home from "./pages/Home.js";
 import Productos from "./pages/Productos.js";
@@ -18,7 +19,7 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />}></Route>
             <Route path="/Productos" element={<Productos />}></Route>
-            {/*<Route path="/contacto" element={<Contacto />}></Route>*/}
+            <Route path="/contacto" element={<Contacto />}></Route>
             <Route path="*" element={<h1>404</h1>}></Route>
           </Route>
         </Routes>
---
+++ src/pages/Contacto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Contacto(props) {
+  return (
+    <div>Contacto</div>
+  )
+}
+
+Contacto.propTypes = {}
+
+export default Contacto
