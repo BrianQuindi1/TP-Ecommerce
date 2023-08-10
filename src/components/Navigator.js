3e9090dd4840b22d42169cde2afd93c620139c2f
BRIAN ISAAC QUINDI <47026829@ort>
2023-08-10 09:07:37 -0300

    Navegacion, productos y detalleProducto

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link, Outlet,NavLink } from 'react-router-dom'
+
 
 function Navigator(props) {
   return (
@@ -9,11 +11,18 @@ function Navigator(props) {
 				{/* responsive-nav */}
 				<div id="responsive-nav">
 					{/* NAV */}
-					<ul className="main-nav nav navbar-nav">
+				<ul className="main-nav nav navbar-nav">
+					<li><NavLink to="/" >Home</NavLink></li>
+					<li><NavLink to="/productos">Productos</NavLink></li>
+					<li><NavLink to="/contacto">Contacto</NavLink></li>
+					<Outlet />
+				</ul>
+				
+					{/*<ul className="main-nav nav navbar-nav">
 						<li className="active"><a href="#">Home</a></li>
 						<li><a href="#">Productos</a></li>
 						<li><a href="#">Contacto</a></li>
-					</ul>
+ 					 </ul>*/}
 					{/* /NAV */}
 				</div>
 				{/* /responsive-nav */}
